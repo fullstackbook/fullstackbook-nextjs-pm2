b9df70fdb7b3ac381b2359909e178dd83121f317
Travis Luong <travis@travisluong.com>
2023-02-12 21:07:54 -0800

    ip

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,20 +1,23 @@
 module.exports = {
-  apps: [{
-    script: 'npm start'
-  }],
+  apps: [
+    {
+      script: "npm start",
+    },
+  ],
 
   deploy: {
     production: {
-      key: 'key.pem',
-      user: 'ubuntu',
-      host: '52.11.235.119',
-      ref: 'origin/main',
-      repo: 'git@github.com:travisluong/my-app.git',
-      path: '/home/ubuntu',
-      'pre-deploy-local': '',
-      'post-deploy': 'source ~/.nvm/nvm.sh && npm install && npm run build && pm2 reload ecosystem.config.js --env production',
-      'pre-setup': '',
-      'ssh_options': 'ForwardAgent=yes'
-    }
-  }
+      key: "key.pem",
+      user: "ubuntu",
+      host: "34.216.107.211",
+      ref: "origin/main",
+      repo: "git@github.com:travisluong/my-app.git",
+      path: "/home/ubuntu",
+      "pre-deploy-local": "",
+      "post-deploy":
+        "source ~/.nvm/nvm.sh && npm install && npm run build && pm2 reload ecosystem.config.js --env production",
+      "pre-setup": "",
+      ssh_options: "ForwardAgent=yes",
+    },
+  },
 };
